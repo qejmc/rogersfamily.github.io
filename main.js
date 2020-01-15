08571f38f305fd58ebc18d1fcd5701b9f83b6650
qejmc <35905481+qejmc@users.noreply.github.com>
2020-01-14 19:53:30 -0700

    V3.0.1

--- main.js
+++ main.js
@@ -1,9 +1,43 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+function displayHB() {
   var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
+  if (x.className === "topnav"){
     x.className += " responsive";
-  } else {
+  } 
+  else{
     x.className = "topnav";
   }
 } 
+
+function homeWarp() {
+	window.open('index.html', '_self');
+}
+
+function infoWarp() {
+	window.open('information.html', '_self');
+}
+
+function codeWarp() {
+	window.open('codecreations.html', '_self');
+}
+
+function signWarp() {
+	window.open('signin.html', '_self');
+}
+
+function javaWarp() {
+	window.open('codecreations/java.html', '_self');
+}
+
+function hcjWarp() {
+	window.open('codecreations/html.html', '_self');
+}
+
+function isValid(){
+	var password = document.getElementById("pword").value;
+	if (password == "2036"){
+		window.open("du13-dd7234sf.html", "_self")
+	}
+	else{
+		window.open("signinfail.html", "_self")
+	}
+}
